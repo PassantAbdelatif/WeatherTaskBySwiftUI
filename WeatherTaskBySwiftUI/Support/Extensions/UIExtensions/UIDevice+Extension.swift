8fbbe6772ef086a639358fcf500141c621d9c1d9
Passant Abdelatif <passantabdelatif@gmail.com>
2023-01-23 20:53:38 +0200

    Update UIDevice+Extension.swift
    fix issue with notch

--- WeatherTaskBySwiftUI/Support/Extensions/UIExtensions/UIDevice+Extension.swift
+++ WeatherTaskBySwiftUI/Support/Extensions/UIExtensions/UIDevice+Extension.swift
@@ -9,11 +9,11 @@ import UIKit
 extension UIDevice {
     /// Returns `true` if the device has a notch
     var hasNotch: Bool {
-        guard #available(iOS 11.0, *), let window = UIApplication.shared.windows.filter({$0.isKeyWindow}).first else { return false }
-        if UIDevice.current.orientation.isPortrait {
-            return window.safeAreaInsets.top >= 44
+        if #available(iOS 11.0, tvOS 11.0, *) {
+            let bottom = UIApplication.shared.keyWindow?.safeAreaInsets.bottom ?? 0
+            return bottom > 0
         } else {
-            return window.safeAreaInsets.left > 0 || window.safeAreaInsets.right > 0
+            return false
         }
     }
 }
